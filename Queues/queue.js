785c44b3fa158e394a151c18eb24eefae9a8d9ad
avbc <avbc@users.noreply.github.com>
2015-04-09 10:24:34 -0400

    Update queue.js

--- Queues/queue.js
+++ Queues/queue.js
@@ -9,124 +9,179 @@
  * ==========================================================================================================================
  */
 
+var randomMultipler = function() {
+    return Math.random() * ( 10 - 1 ) + 1;
+};
+
+var numRequests = 0;
+
 // ------ Begin enqueue -------
-var enqueue = function(url, callback) {
-  var $this = this,
+var enqueue = function( url, callback ) {
+    var $this = this,
 
-  // The callback to execute when we're ready...
-  onExecuteCallback = function() {
+    // The callback to execute when we're ready...
+    onExecuteCallback = function() {
+        request( url, function() {
+            callback.apply( $this, arguments );
 
-    request(url, function() {
-      callback.apply($this, arguments);
-  
-      // as async of a continue as we can get for now...
-      setTimeout(function() {
-        RequestQueue.status = 0;
-        RequestQueue.pop();
-      }, 20);
-    });
-  };
+            // as async of a continue as we can get for now...
+            setTimeout( function() {
+                RequestQueue.status = 0;
+                RequestQueue.pop();
+            }, 20 );
+        });
+    };
 
-  requestItem = new RequestQueueItem(onExecuteCallback);
-  RequestQueue.push(requestItem).pop();
+    requestItem = new RequestQueueItem( onExecuteCallback );
+    RequestQueue.push( requestItem ).pop();
 };
 
-// Process Request Queue Manager...
-(function() {
+var request = function( url, callback ) { 
+    var staticSecondMultiplier = 1000;
+    var randomTimeoutMultipler = randomMultipler();
+    var mockRequestLag = Math.floor( randomTimeoutMultipler * staticSecondMultiplier );
 
-  var $outterScope = this,
-  IDLE = 0, PROCESSING = 1, REQUEST_IN_PROGRESS = 2,
+    numRequests++;
 
-  isFunction = function(obj) { return toString.call(obj) == '[object Function]'; },
-  resetRequestQueue = function() {
-      RequestQueue.status = IDLE;
-      kickstarted = false;
-  };
+    setTimeout( function() {
+        callback({
+            a: 1,
+            b: 2,
+            requestNum: numRequests
+        });
+    }, mockRequestLag );
+};
 
-  $outterScope.RequestInProgress = false;
+// Begin definitions used for testing purposes only
+var reqCount = 1;
+var reqCallback;
 
-  // The Actual Request Queue Item object...
-  function RequestQueueItem(onExecuteCallback) { this.callback = onExecuteCallback; }
+var runTest = function( numTimes ) {
+    var i;
+    var z = numTimes * 10;
 
-  RequestQueueItem.prototype = {
-    execute: function() {
-      if(isFunction(this.callback)) this.callback();
+    for( i = 0; i < ; i++, ++reqCount ) {
+        console.log( "issueing enqueue() " + reqCount + " of " + ( reqCount / 10 * 10 ) );
+
+        enqueue( "http://www.example.com", function( data ) {
+            console.log( "Recieved Callback for request with data:" );
+            console.log( data );
+        });
     }
-  };
+};
+// End definitions used for testing purposes only
 
-  $outterScope.RequestQueueItem = RequestQueueItem;
+( function() {
+    'use strict';
 
-  // A Process Queue for handling Queued Requests.
-  var RequestQueue = function(obj) {
-    if(obj instanceof RequestQueue) return obj;
-    if(!(this instanceof RequestQueue)) return new RequestQueue;
-    this.RQ = obj;  // in case we need it...
-  };
+    var RequestQueue;
+    var $outterScope = this;
 
-  $outterScope.RequestQueue = RequestQueue;
-  RequestQueue.queue = [];
-  RequestQueue.status = status;
+    // Enums
+    var IDLE = 0;
+    var PROCESSING = 1;
+    var REQUEST_IN_PROGRESS = 2,
 
-  RequestQueue.isIdle = function() { return RequestQueue.status == IDLE; };
-  RequestQueue.size = function() { return RequestQueue.queue.length; };
-  RequestQueue.isEmpty = function() { return RequestQueue.size() < 1; };
+    var isFunction = function( obj ) {
+        return toString.call( obj ) == '[object Function]';
+    };
 
-  RequestQueue.push = function(requestItem) {
-    if(!requestItem || !requestItem instanceof RequestQueueItem) return;
+    var resetRequestQueue = function() {
+        RequestQueue.status = IDLE;
+        kickstarted = false;
+    };
 
-    RequestQueue.queue.push(requestItem);
-    return this;
-  };
+    var isRequestQueueInstance = function( obj ) {
+        return ( obj instanceof RequestQueue );
+    };
 
-  RequestQueue.pop = function() {
-    if(RequestQueue.status == PROCESSING) return;
+    $outterScope.RequestInProgress = false;
 
-    RequestQueue.status = PROCESSING;
-
-    if(RequestQueue.size() < 1) {
-      resetRequestQueue();
-      RequestQueue.status = IDLE;
-      return;
+    // The Actual Request Queue Item object...
+    function RequestQueueItem( onExecuteCallback ) {
+        this.callback = onExecuteCallback;
     }
 
-    var n = RequestQueue.size()-1, 
-    requestItem = null;
+    RequestQueueItem.prototype = {
+        execute: function() {
+            if( isFunction( this.callback ) ) {
+                this.callback();
+            }
+        }
+    };
 
-    do {
-      requestItem = RequestQueue.queue.shift();
-      if(requestItem && requestItem instanceof RequestQueueItem) {
-        requestItem.execute();
-        break;
-      }
-    } while(n--);
-  };
-}).call(this);
+    $outterScope.RequestQueueItem = RequestQueueItem;
 
-var numRequests = 0,
+    // A Process Queue for handling Queued Requests.
+    RequestQueue = function( obj ) {
+        if( obj instanceof RequestQueue ) {
+            return obj;
+        }
 
-request = function(url, callback) { 
-  var staticSecondMultiplier = 1000,
-  randomTimeoutMultipler = Math.random() * (10 - 1) + 1,
-  mockRequestLag = Math.floor(randomTimeoutMultipler * staticSecondMultiplier);
+        if( !( this instanceof RequestQueue ) ) {
+            return new RequestQueue;
+        }
+    
+        this.RQ = obj;  // in case we need it...
+    };
 
-  numRequests++;
+    $outterScope.RequestQueue = RequestQueue;
 
-  setTimeout(function() {
-    callback( { a: 1, b: 2, requestNum: numRequests});
-  }, mockRequestLag);
-};
+    RequestQueue.queue = [];
+    RequestQueue.status = status;
 
-// Uncomment to Test ...
-var reqCallback, reqCount = 1, i,
+    RequestQueue.isIdle = function() {
+        return RequestQueue.status == IDLE;
+    };
 
-runTest = function(numTimes) {
-  for(i = 0; i < numTimes*10; i++, ++reqCount) {
-    console.log("issueing enqueue() " + reqCount + " of " + reqCount/10*10);
-    enqueue("http://www.example.com", function(data) {
-      console.log("Recieved Callback for request with data:");
-      console.log(data);
-    });
-  }
-};
+    RequestQueue.size = function() {
+        return RequestQueue.queue.length;
+    };
 
-runTest(1);
+    RequestQueue.isEmpty = function() {
+        return RequestQueue.size() < 1;
+    };
+
+    RequestQueue.push = function( requestItem ) {
+        if( !requestItem || !requestItem instanceof RequestQueueItem ) {
+            return;
+        }
+
+        RequestQueue.queue.push( requestItem );
+
+        return this;
+    };
+
+    RequestQueue.pop = function() {
+        var n, requestItem;
+
+        if( RequestQueue.status == PROCESSING ) {
+            return;
+        }
+
+        RequestQueue.status = PROCESSING;
+
+        if( RequestQueue.size() < 1 ) {
+            resetRequestQueue();
+            RequestQueue.status = IDLE;
+
+            return;
+        }
+
+        n = RequestQueue.size() - 1;
+        requestItem = null;
+
+        do {
+            requestItem = RequestQueue.queue.shift();
+
+            if( requestItem && requestItem instanceof RequestQueueItem ) {
+                requestItem.execute();
+                break;
+            }
+        } while( n-- );
+    };
+}).call( this );
+
+
+// uncomment to run tests...
+// runTest(1);
